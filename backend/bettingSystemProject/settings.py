86a7ca44153077aa6baa87c760f296d43542cae9
dennis <dionisisvaskr@hotmail.com>
2026-03-06 10:42:27 +0200

    switch to postgres

--- backend/bettingSystemProject/settings.py
+++ backend/bettingSystemProject/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 from celery.schedules import crontab
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -113,11 +114,17 @@ CORS_ALLOWED_ORIGINS = [
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3')
+    )
 }
 
 
