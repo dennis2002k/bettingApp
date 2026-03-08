b8c2867895ce6475513442f479701a5daee07207
dennis <dionisisvaskr@hotmail.com>
2026-03-08 12:52:03 +0200

    fixed register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Bets from './pages/Bets';
+import Register from './pages/Register';
 
 export default function App() {
   return (
